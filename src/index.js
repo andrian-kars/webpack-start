c645671d9f4ed15eccee9937e35462a7cd278481
andrian-kars <andrian.karsanashvili@gmail.com>
2021-04-22 19:45:42 +0300

    :heavy_plus_sign: file loader (imgs & fonts)

--- src/index.js
+++ src/index.js
@@ -1,6 +1,10 @@
 import { Post } from './Post'
+import json from './assets/json'
+import logo from './assets/webpack-logo.png'
 import './styles/style.css'
 
-const post = new Post('Webpack Post Title')
+const post = new Post('Webpack Post Title', logo)
 
 console.log('Post to String:', post.toString())
+
+console.log('JSON:', json)
